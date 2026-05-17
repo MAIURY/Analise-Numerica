1317fdcb71592d7edfeaae8ce2a6a27fe0c20698
Maiury <fernandesmaiury@gmail.com>
2026-05-17 10:44:58 -0300

    Melhora bisseccao e atualiza README

--- utils.py
+++ utils.py
@@ -4,8 +4,3 @@ import math
 def teorema_bolzano(funcao, a, b):
     # Retorna True se houver mudanca de sinal no intervalo [a, b]
     return funcao(a) * funcao(b) < 0
-
-
-def derivada_aproximada(funcao, x, h=1e-6):
-    # Diferenca central para aproximar a derivada
-    return (funcao(x + h) - funcao(x - h)) / (2 * h)
